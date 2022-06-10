e6aaf198e01c95b20d6c526decb0a06847c07ece
Teresa Hanak <teresahanak@hotmail.com>
2022-06-10 18:42:03 -0500

    Finished data collection

--- wikipedia_notable_deaths/pipelines.py
+++ wikipedia_notable_deaths/pipelines.py
@@ -24,14 +24,14 @@ class SQLitePipeline:
 
     def open_spider(self, spider):
         # create database file
-        self.connection = sqlite3.connect('refs2.db')
+        self.connection = sqlite3.connect('refs5.db')
         # we need a cursor object to execute SQL queries
         self.c = self.connection.cursor()
         #  try/except will help when running this for the +2nd time (we can't create the same table twice)
         try:
             # query: create table with columns
             self.c.execute('''
-                CREATE TABLE refs2(
+                CREATE TABLE refs5(
                     link TEXT,
                     num_references TEXT
                 )
@@ -47,7 +47,7 @@ class SQLitePipeline:
     def process_item(self, item, spider):
         # query: insert data into table
         self.c.execute('''
-            INSERT INTO refs2 (link, num_references) VALUES(?,?)
+            INSERT INTO refs5 (link, num_references) VALUES(?,?)
         ''', (
             item.get('link'),
             item.get('num_references')
